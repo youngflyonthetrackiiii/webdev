bf9e2239ab8cc2029c50177ac363cf460e4a193d
Scholar <scholar@paradigm.local>
2023-11-07 14:59:40 -0700

    vars

---
+++ intro.js
@@ -0,0 +1,32 @@
+const myName = "leagden"
+
+let favColor = "red"
+
+favColor = "red"
+
+console.log(favColor)
+
+let age = 30
+
+mainperson = {
+    name: ";leagden",
+    age: 30,
+    favColor: "red",
+}
+
+
+favThings = [2, "movies"]
+
+const isAlive = true
+
+function describeperson(person) {
+    alert("MISSING PERSON Alert: " + {person.name})
+}
+
+describeperson(mainperson)
+
+if (age >=30){
+    alert("your on your way to middle age.");
+} else if (age < 30){
+    alert("your a baby.")
+}
---
+++ variable.js
@@ -0,0 +1,16 @@
+let text1 = "i am not leagden"
+let text2 = "i am leagden"
+let text3 = "you are the president of the united states of america"
+let list1 = [1,2,3]
+let num1 = 3
+let num2 = 7
+let num3 = 2
+Boolean(10>9)
+Boolean(7<13)
+Boolean(9999>1)
+let bool1 = false
+let bool2 = true 
+let bool3 = Boolean(10>9)
+let person1 = "mr.hunt"; 
+let person2 = "leagden";
+let person3 = "archer";
